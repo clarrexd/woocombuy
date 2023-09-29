7c3fc3e04b722aa2406b3db9ea643994ad96503f
Clarence Renström <clarre.renstrom@gmail.com>
2023-09-30 00:21:53 +0200

    installed plugins, testing db export

---
+++ plugins/klarna-payments-for-woocommerce/vendor/composer/installed.php
@@ -0,0 +1,43 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'krokedil/woocommerce' => array(
+            'pretty_version' => 'dev-develop-sub-free-trial',
+            'version' => 'dev-develop-sub-free-trial',
+            'reference' => 'a0b5469a2e0d08f8b4da4881f105aa6a3ad4e7a5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../krokedil/woocommerce',
+            'aliases' => array(
+                0 => '1.2.2',
+            ),
+            'dev_requirement' => false,
+        ),
+        'krokedil/wp-api' => array(
+            'pretty_version' => '1.0.1',
+            'version' => '1.0.1.0',
+            'reference' => '5da83b450809821a80e2d1891da3bbb90c42d111',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../krokedil/wp-api',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
